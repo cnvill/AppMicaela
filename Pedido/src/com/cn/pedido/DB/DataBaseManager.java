4561d2c7f3468c855e236a85dae469f6650e3c28
cnvill <cn_vill@hotmail.com>
2015-04-10 00:31:46 +0000

    Funtionality User Login

---
+++ Pedido/src/com/cn/pedido/DB/DataBaseManager.java
@@ -0,0 +1,71 @@
+package com.cn.pedido.DB;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by CN on 19/10/2014.
+ */
+public class DataBaseManager {
+
+    private DbHelper helper;
+    private SQLiteDatabase db;
+
+    public DataBaseManager(Context ctx){
+        helper= new DbHelper(ctx);
+        db=helper.getReadableDatabase();
+    }
+   
+    //Create Table Parameter
+
+    public static final String tableNameParameter="tparameter";
+    public static final String pId="_id";
+    public static final String pCode="codigo";
+    public static final String pValue="valor";
+    public static final String pEstado="estado";
+
+
+    public static final String createTableParameter="create table "+tableNameParameter+ "("
+            + pId + " integer primary key autoincrement, "
+            + pCode + " text not null, "
+            + pValue+ " text not null, "
+            + pEstado +" integer );";
+
+    public ContentValues generarParameterContentValues(String code, String value){
+        ContentValues valores= new ContentValues();
+        valores.put(pCode, code);
+        valores.put(pValue, value);
+        valores.put(pEstado, "1");
+        return valores;
+    }
+
+    public void insertarParameter(String code, String value){
+        db.insert(tableNameParameter, null ,generarParameterContentValues(code, value));
+    }
+
+    public boolean GetConfigNotify(String code){
+        boolean respt=false;
+        String[] columnas= new String[]{ pId, pCode, pValue, pEstado};
+        Cursor c= db.query(tableNameParameter, columnas, pCode+"=?", new String[]{ code }, null, null, null);
+        if(c.moveToFirst())
+            respt=true;
+            
+        return respt;   
+    }
+
+    public String GetConfigNotifyOption(String code){
+        String respt="";
+        String[] columnas= new String[]{ pId, pCode, pValue, pEstado};
+        Cursor c= db.query(tableNameParameter, columnas, pCode+"=?", new String[]{ code }, null, null, null);
+        if(c.moveToFirst())
+            respt = c.getString(2);
+            
+        return respt;   
+    }
+
+    //End Table Parameter 
+   
+    
+}
--- Pedido/src/com/cn/pedido/LoginActivity.java
+++ Pedido/src/com/cn/pedido/LoginActivity.java
@@ -20,29 +20,165 @@ import org.apache.http.entity.BufferedHttpEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.apache.http.client.methods.*;
+import org.apache.http.message.*;
+import org.apache.http.client.entity.*;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.*;
 import org.apache.http.*;
 import android.content.*;
 import java.util.UUID;
 import java.text.*;
+import com.cn.pedido.DB.DataBaseManager;
+import android.database.Cursor;
+import android.content.DialogInterface;
+import java.util.UUID;
 
 public class LoginActivity extends Activity
 {
+
+	private DataBaseManager manager;
+    Cursor cursor;
+	String phone;
+	EditText txtName, txtPhone;
 
 	@Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+
+        manager= new DataBaseManager(this);
+
+		if(manager.GetConfigNotify("config"))
+		{
+			//Phone
+			phone = manager.GetConfigNotifyOption("config");
+			if(ValidPhone(phone).equalsIgnoreCase("ok")){
+				Intent mainActivity= new Intent(this, MainActivity.class);
+	           startActivity(mainActivity);
+			}			
+		}
+		else
+		{
+			setContentView(R.layout.login);
+			txtName=(EditText)findViewById(R.id.txtName);    
+			txtPhone=(EditText)findViewById(R.id.txtPhone);    
+		}
 		
 	}
-	
 	public void onLogin(View v)
 	{
-		
+		if(txtName.getText().length()==0){
+				Toast.makeText(getApplicationContext(), "Complete el campo nombre", Toast.LENGTH_LONG).show();
+				txtName.setFocusable(true);
+				return;
+			}
+
+		if(txtPhone.getText().length()==0){
+				Toast.makeText(getApplicationContext(), "Complete el campo Nº Celular", Toast.LENGTH_LONG).show();
+				txtPhone.setFocusable(true);
+				return;
+			}
+
+		if(ValidPhone(txtPhone.getText().toString()).equalsIgnoreCase("ok")){
+			Toast.makeText(getApplicationContext(), "El Nº Celular ya se encuentra registrado", Toast.LENGTH_LONG).show();
+			return;
+		}else
+		{
+			try{
+
+				UUID idusuario = UUID.randomUUID();
+
+				StrictMode.ThreadPolicy policy= new StrictMode.ThreadPolicy.Builder().permitAll().build();
+				StrictMode.setThreadPolicy(policy);
+
+				HttpClient oHttpclientRegist= new DefaultHttpClient();
+				HttpPost oHttpPostRegist = new HttpPost("http://loswaykis.com/ws/wsusuarioinsert.php");
+				List<BasicNameValuePair> namevalue= new ArrayList<BasicNameValuePair>();	
+
+				namevalue.add(new BasicNameValuePair("idusuario", idusuario.toString()));
+				namevalue.add(new BasicNameValuePair("name", txtName.getText().toString() ));
+				namevalue.add(new BasicNameValuePair("phone", txtPhone.getText().toString() ));
+
+				oHttpPostRegist.setEntity(new UrlEncodedFormEntity(namevalue));
+				HttpResponse responseRegist=oHttpclientRegist.execute(oHttpPostRegist);
+				if(responseRegist.getStatusLine().getStatusCode()== HttpStatus.SC_OK){
+
+					HttpEntity entity = responseRegist.getEntity();
+					BufferedHttpEntity buffer = new BufferedHttpEntity(entity);
+					InputStream iStream = buffer.getContent();
+
+					String aux = "";
+
+					BufferedReader r = new BufferedReader(new InputStreamReader(iStream));
+					String line;
+					while ((line = r.readLine()) != null) {
+						aux += line;
+					}
+
+					JSONObject jsonObject = new JSONObject(aux);
+					JSONArray result = jsonObject.getJSONArray("result");
+
+					if(result.getJSONObject(0).getString("status").equalsIgnoreCase("ok")){
+						Toast.makeText(getApplicationContext(), "Se registro correctamente", Toast.LENGTH_LONG).show();
+						Intent mainActivity= new Intent(this, MainActivity.class);
+						startActivity(mainActivity);
+					}else{
+						Toast.makeText(getApplicationContext(), "No se llego a registrar intentende nuevo", Toast.LENGTH_LONG).show();
+					}
+				}else{
+					Toast.makeText(getApplicationContext(), "Ocurrio problema conexión con la red", Toast.LENGTH_LONG).show();
+				}	
+			}catch(Exception ex){
+				Toast.makeText(getApplicationContext(), "Ocurrio problema conexión con la red", Toast.LENGTH_LONG).show();
+				
+			}
+			
+		}
+
+			
+	}
+
+	public String ValidPhone(String phone){
+		String resp="No";
+		try{
+			StrictMode.ThreadPolicy policy= new StrictMode.ThreadPolicy.Builder().permitAll().build();
+			StrictMode.setThreadPolicy(policy);
+			
+			HttpClient oHttpclient= new DefaultHttpClient();
+			HttpPost oHttpPost = new HttpPost("http://loswaykis.com/ws/wslogin.php");
+			List<BasicNameValuePair> namevaluePairs= new ArrayList<BasicNameValuePair>();		
+			namevaluePairs.add(new BasicNameValuePair("phone", phone));
+
+			oHttpPost.setEntity(new UrlEncodedFormEntity(namevaluePairs));
+			HttpResponse response=oHttpclient.execute(oHttpPost);
+			if(response.getStatusLine().getStatusCode()== HttpStatus.SC_OK){
+				
+					HttpEntity entity = response.getEntity();
+					BufferedHttpEntity buffer = new BufferedHttpEntity(entity);
+					InputStream iStream = buffer.getContent();
+
+					String aux = "";
+
+					BufferedReader r = new BufferedReader(new InputStreamReader(iStream));
+					String line;
+					while ((line = r.readLine()) != null) {
+						aux += line;
+					}
+
+					JSONObject jsonObject = new JSONObject(aux);
+					JSONArray result = jsonObject.getJSONArray("result");
+
+					resp = result.getJSONObject(0).getString("status");
+			}
+
+		}catch(Exception ex){
+			resp= ex.getMessage();
+		}
+		return resp;
+
 	}
 }
